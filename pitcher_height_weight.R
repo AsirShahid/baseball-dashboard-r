766764b1ac712c49587160a369a3d1b52a0d5c1c
Asir Shahid <asirshahid@protonmail.com>
2023-05-20 15:20:33 -0400

    Removed lbs from weight column

--- pitcher_height_weight.R
+++ pitcher_height_weight.R
@@ -1,4 +1,11 @@
+library(shiny)
+library(mlbplotR)
+library(ggplot2)
+library(dplyr)
+library(baseballr)
+library(scales)
 library(rvest)
+library(tidyr)
 library(dplyr)
 
 # Create an empty list to store data frames
@@ -6,15 +13,6 @@ all_data <- list()
 
 # Define the vector of team names and abbreviations
 teams <- c("ari", "atl", "bal", "bos", "chc", "chw", "cin", "cle", "col", "det", "hou", "kc", "laa", "lad", "mia", "mil", "min", "nym", "nyy", "oak", "phi", "pit", "sd", "sea", "sf", "stl", "tb", "tex", "tor", "wsh")
-
-# Function to convert height from feet-inches to inches
-convert_height <- function(height) {
-  height_parts <- strsplit(height, "-")[[1]]
-  feet <- as.numeric(height_parts[1])
-  inches <- as.numeric(height_parts[2])
-  total_inches <- feet * 12 + inches
-  return(total_inches)
-}
 
 # Loop over each team
 for (team in teams) {
@@ -37,5 +35,37 @@ for (team in teams) {
 # Combine all the data frames in the list into a single data frame
 combined_df <- bind_rows(all_data)
 
+# Install tidyr and dplyr if you haven't done so already
+if (!require(tidyr)) {
+  install.packages("tidyr")
+}
+if (!require(dplyr)) {
+  install.packages("dplyr")
+}
+
+# Load tidyr and dplyr
+library(tidyr)
+library(dplyr)
+
+# Then continue with your existing code
+combined_df <- combined_df %>%
+  mutate(HT = gsub("'", " ", HT),  # replace the ' character with a space
+         HT = gsub("\"", "", HT),  # remove the " character
+         WT = gsub(" lbs", "", WT)) %>%  # remove " lbs" from the weight
+  separate(HT, into = c("feet", "inches"), sep = " ", extra = "drop") %>%
+  mutate(feet = as.numeric(feet),
+         inches = as.numeric(inches),
+         HT_in_inches = feet*12 + inches) %>%
+  select(-feet, -inches)  # remove the temporary columns
+
+# Convert the height and weight columns to numeric
+combined_df <- combined_df %>%
+  mutate(HT_in_inches = as.numeric(HT_in_inches),
+         WT = as.numeric(WT))
+
+# remove the numbers from the name column
+combined_df$Name <- gsub("\\d", "", combined_df$Name)
+
 # View the combined data frame
 head(combined_df)
+
